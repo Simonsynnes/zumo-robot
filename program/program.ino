8d00a17a3124af876d0522b5ec4ebcc1783fcfac
unknown <jonoie95@gmail.com>
2016-11-07 15:22:19 +0100

    Endret hastighet.

--- program/program.ino
+++ program/program.ino
@@ -12,7 +12,7 @@
 // these might need to be tuned for different motor types
 #define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
 #define TURN_SPEED        200
-#define FORWARD_SPEED     200
+#define FORWARD_SPEED     400
 #define REVERSE_DURATION  200 // ms
 #define TURN_DURATION     300 // ms
  
