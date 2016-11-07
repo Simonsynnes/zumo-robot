07e8f9afae691824910f602845fe662f18f07b4d
Jonathan Øie <jonoie95@gmail.com>
2016-11-07 17:52:11 +0100

    Code structure.

--- program/program.ino
+++ program/program.ino
@@ -1,97 +1,57 @@
-#include <ZumoBuzzer.h>
-#include <ZumoMotors.h>
-#include <Pushbutton.h>
-#include <QTRSensors.h>
-#include <ZumoReflectanceSensorArray.h>
- 
-#define LED 13
- 
-// this might need to be tuned for different lighting conditions, surfaces, etc.
-#define QTR_THRESHOLD  1000 // microseconds
-  
-// these might need to be tuned for different motor types
-#define REVERSE_SPEED     200 // 0 is stopped, 400 is full speed
-#define TURN_SPEED        200
-#define FORWARD_SPEED     400
-#define REVERSE_DURATION  200 // ms
-#define TURN_DURATION     300 // ms
- 
-ZumoBuzzer buzzer;
-ZumoMotors motors;
-Pushbutton button(ZUMO_BUTTON); // pushbutton on pin 12
- 
-#define NUM_SENSORS 6
-unsigned int sensor_values[NUM_SENSORS];
- 
-ZumoReflectanceSensorArray sensors(QTR_NO_EMITTER_PIN);
+// SWITCH CASES -------------------------
+const int case_normal = 0; // Look for opponent
+const int case_chase = 1; // Opponent found
+const int case_boundary_detected = 2; // Robot is standing on track limit
+// --------------------------------------
 
-void waitForButtonAndCountDown()
+void setup() 
 {
-  digitalWrite(LED, HIGH);
-  button.waitForButton();
-  digitalWrite(LED, LOW);
-   
-  // play audible countdown
-  for (int i = 0; i < 3; i++)
-  {
-    delay(1000);
-    buzzer.playNote(NOTE_G(3), 200, 15);
-  }
-  delay(1000);
-  buzzer.playNote(NOTE_G(4), 500, 15);  
-  delay(1000);
-}
- 
-void setup()
-{
-  Serial.begin(9600);
-  // uncomment if necessary to correct motor directions
- // motors.flipLeftMotor(true);
- //motors.flipRightMotor(true);
-   
-  pinMode(LED, HIGH);
-   
-  waitForButtonAndCountDown();
+
 }
 
-void loop()
+void loop() 
+{
+  // Get all sensor data
+  int lightSensorLeftValue = 0;
+  int lightSensorRightValue = 0;
+  int distanceSensorValue = 0;
+  
+  // Create action based on sensor data
+  int action = validateSensorData(lightSensorLeftValue, lightSensorRightValue, distanceSensorValue);
+  
+  // Perform action based on sensor data
+  switch (action)
+  {
+    case case_normal:
+		normal();
+		break;
+    
+    case case_chase:
+		chase();
+		break;
+    
+    case case_boundary_detected:
+		boundaryDetected();
+		break;
+  }
+}
+
+int validateSensorData(int lightLeft, int lightRight, int distance)
 {
   
-  if (button.isPressed())
-  {
-    // if button is pressed, stop and wait for another press to go again
-    motors.setSpeeds(0, 0);
-    button.waitForRelease();
-    waitForButtonAndCountDown();
-  }
-   
+}
 
-  sensors.read(sensor_values);
+void chase()
+{
 
-  Serial.println(sensor_values[0]); 
-    Serial.println(sensor_values[5]); 
+}
+
+void normal()
+{
   
-  if (sensor_values[0] > QTR_THRESHOLD)
-  {
-    // if leftmost sensor detects line, reverse and turn to the right
-    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-    delay(REVERSE_DURATION);
-    motors.setSpeeds(TURN_SPEED, -TURN_SPEED);
-    delay(TURN_DURATION);
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-  }
-  else if (sensor_values[5] > QTR_THRESHOLD)
-  {
-    // if rightmost sensor detects line, reverse and turn to the left
-    motors.setSpeeds(-REVERSE_SPEED, -REVERSE_SPEED);
-    delay(REVERSE_DURATION);
-    motors.setSpeeds(-TURN_SPEED, TURN_SPEED);
-    delay(TURN_DURATION);
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-  }
-  else
-  {
-    // otherwise, go straight
-    motors.setSpeeds(FORWARD_SPEED, FORWARD_SPEED);
-  }
+}
+
+void boundaryDetected()
+{
+
 }
